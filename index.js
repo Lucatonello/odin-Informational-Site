7f447c26315a6132f65e1b51124ab6d595bfb1fc
Luca <lucatonello097@gmail.com>
2024-07-27 11:21:40 -0300

    translated code to Express

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var path = require('path');
+const path = require('path');
+const express = require("express");
+const app = express();
 
-http.createServer(function(req, res) {
-    var q = url.parse(req.url, true);
-    const files = [
-        '/',
-        '/about',
-        '/contact-me'
-    ];
-    var fileName = './404.html';
-    if (files.includes(q.pathname)) {
-        fileName = q.pathname === '/' ? './index.html' : "." + q.pathname + '.html';
-    }
+const PORT = 8080;
 
-    fs.readFile(fileName, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 not FOUND");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact-me.html'));
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
